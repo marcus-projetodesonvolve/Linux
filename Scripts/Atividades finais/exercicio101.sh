f8beedb6fb4fa421d296a7bd19c6ee8a8c3f522b
marcus-projetodesonvolve <marcus.santos@pdbomdespacho.com.br>
2025-06-16 17:35:01 -0300

    Delete Scripts/Atividades finais/exercicio101.sh

--- Scripts/Atividades finais/exercicio101.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Conta o número de linhas da saída do comando 'ps aux',
-# que lista todos os processos em execução.
-# Subtraímos 1 para não contar a linha do cabeçalho.
-quantidade_processos=$(ps aux | wc -l)
-quantidade_processos=$((quantidade_processos - 1))
-
-echo "Atualmente, existem $quantidade_processos processos em execução neste sistema."
