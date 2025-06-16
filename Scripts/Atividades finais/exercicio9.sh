a6e27536d1626cd09ebc1f16d0f0431680e9eb41
marcus-projetodesonvolve <marcus.santos@pdbomdespacho.com.br>
2025-06-16 17:08:45 -0300

    Update exercicio9.sh

--- Scripts/Atividades finais/exercicio9.sh
+++ Scripts/Atividades finais/exercicio9.sh
@@ -13,9 +13,8 @@ while [ "$adivinhou" == false ]; do
     read -p "Sua adivinhação: " palpite
     tentativas=$(( tentativas + 1 ))
 
-    if ! [[ "$palpite" =~ ^[0-9]+$ ]]; then
-        echo "Por favor, digite um número válido."
-    elif [ "$palpite" -lt "$numero_secreto" ]; then
+
+    if [ "$palpite" -lt "$numero_secreto" ]; then
         echo "Muito baixo! Tente um número maior."
     elif [ "$palpite" -gt "$numero_secreto" ]; then
         echo "Muito alto! Tente um número menor."
